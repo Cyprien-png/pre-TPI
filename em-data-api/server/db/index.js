e0e9f2c78bcbd0b0df8fa08b24d8abd29ededb4b
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2023-03-07 10:17:23 +0100

    Adding env
    Creating a profile for the api to access EM db and adding the credentials in a .env file

--- em-data-api/server/db/index.js
+++ em-data-api/server/db/index.js
@@ -1,24 +1,27 @@
+require('dotenv').config()
+
 const mysql = require('mysql')
 
-mysql.createPool({
+const pool = mysql.createPool({
     connectionLimit: 10,
-    password: "blabla",
-    user: "chripr",
-    database: "elite-motion",
-    host: "localhost",
-    port: "3306"
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
 })
 
 let chriprdb = {}
 
 chriprdb.all = () => {
-
     return new Promise((resolve, reject) => {
-
-
-
+        pool.query("SELECT * FROM users", (err, results) => {
+            if (err) {
+                return reject(err)
+            }
+            return resolve(results)
+        })
     })
-
 }
 
 module.exports = chriprdb
